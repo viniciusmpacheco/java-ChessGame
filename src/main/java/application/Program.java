ab695607ed664db60c474b95aacbd0264593060d
Vinicius Pacheco <vinicius.mpacheco@hotmail.com>
2021-05-03 22:18:53 -0300

    Game initialization

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,6 +1,63 @@
 package main.java.application;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import main.java.game.ChessMatch;
+import main.java.game.ChessPiece;
+import main.java.game.ChessPosition;
+import main.java.game.exception.ChessException;
+
 public class Program {
-	public static void main(String[] args) {
-		System.out.print("Hello World!");
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while (!chessMatch.getCheckMate()) {
+			try {
+				ChessUI.clearScreen();
+				ChessUI.printMatch(chessMatch, captured);
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = ChessUI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				ChessUI.clearScreen();
+				ChessUI.printBoard(chessMatch.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = ChessUI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
+		ChessUI.clearScreen();
+		ChessUI.printMatch(chessMatch, captured);
 	}
 }
